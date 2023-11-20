c141b967f8eda8d947b2d71b602713a637c7a0b2
Ibrahim Khaled <41121509+IbrahimKhaled19@users.noreply.github.com>
2023-11-20 14:26:09 +0200

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-function toggleSearchBar() {
-    var searchBarContainer = document.getElementById("search-bar-container");
-    searchBarContainer.style.display = (searchBarContainer.style.display === "none" || searchBarContainer.style.display === "") ? "flex" : "none";
-    searchBarContainer.classList.toggle("fade-in");
-}
